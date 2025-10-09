00d5d4c6afa58a5c949bbc533caf3b8814d6727d
yanbo <yanbo@hotmail.com>
2025-10-09 14:41:31 +0800

    fix: Replace inuse wrapper with realtime wrapper to match CMakeLists.txt
    - CMakeLists.txt expects solveGIKStepRealtime.cpp and realtime wrapper
    - Previously deployed inuse wrapper from backup (wrong version)
    - Now using correct realtime wrapper files from backup
    - Files: solveGIKStepRealtime.cpp/h + 4 realtime wrapper support files

---
+++ ros2/gik9dof_solver/matlab_codegen/include/gik9dof_codegen_realtime_solveGIKStepWrapper_data.cpp
@@ -0,0 +1,57 @@
+//
+// File: gik9dof_codegen_realtime_solveGIKStepWrapper_data.cpp
+//
+// MATLAB Coder version            : 24.2
+// C/C++ source code generated on  : 06-Oct-2025 17:03:24
+//
+
+// Include Files
+#include "gik9dof_codegen_realtime_solveGIKStepWrapper_data.h"
+#include "rt_nonfinite.h"
+
+// Variable Definitions
+namespace gik9dof {
+omp_nest_lock_t gik9dof_codegen_realtime_solveGIKStepWrapper_nestLockGlobal;
+
+const signed char iv[16]{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
+
+const char cv1[9]{'p', 'r', 'i', 's', 'm', 'a', 't', 'i', 'c'};
+
+const char cv2[11]{'b', 'a', 's', 'e', '_', 'l', 'i', 'n', 'k', '_', 'x'};
+
+const char cv3[11]{'b', 'a', 's', 'e', '_', 'l', 'i', 'n', 'k', '_', 'y'};
+
+const char cv4[21]{'a', 'b', 's', 't', 'r', 'a', 'c', 't', '_', 'c', 'h',
+                   'a', 's', 's', 'i', 's', '_', 'l', 'i', 'n', 'k'};
+
+const char cv5[18]{'l', 'e', 'f', 't', '_', 'a', 'r', 'm', '_',
+                   'b', 'a', 's', 'e', '_', 'l', 'i', 'n', 'k'};
+
+const char cv6[14]{'l', 'e', 'f', 't', '_', 'a', 'r',
+                   'm', '_', 'l', 'i', 'n', 'k', '1'};
+
+const char cv7[14]{'l', 'e', 'f', 't', '_', 'a', 'r',
+                   'm', '_', 'l', 'i', 'n', 'k', '2'};
+
+const char cv8[14]{'l', 'e', 'f', 't', '_', 'a', 'r',
+                   'm', '_', 'l', 'i', 'n', 'k', '3'};
+
+const char cv9[14]{'l', 'e', 'f', 't', '_', 'a', 'r',
+                   'm', '_', 'l', 'i', 'n', 'k', '4'};
+
+const char cv10[14]{'l', 'e', 'f', 't', '_', 'a', 'r',
+                    'm', '_', 'l', 'i', 'n', 'k', '5'};
+
+const char cv11[14]{'l', 'e', 'f', 't', '_', 'a', 'r',
+                    'm', '_', 'l', 'i', 'n', 'k', '6'};
+
+const char cv12[17]{'l', 'e', 'f', 't', '_', 'g', 'r', 'i', 'p',
+                    'p', 'e', 'r', '_', 'l', 'i', 'n', 'k'};
+
+} // namespace gik9dof
+
+//
+// File trailer for gik9dof_codegen_realtime_solveGIKStepWrapper_data.cpp
+//
+// [EOF]
+//
